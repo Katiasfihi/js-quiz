ce2803a980d698b38fd92ac920eff96a4a480563
Katiasfihi <Katiasfihi@users.noreply.github.com>
2021-02-18 09:50:02 +0000

    3rd commit - arrays - context

--- Experiments.js
+++ Experiments.js
@@ -116,4 +116,7 @@ const daysOfTheWeek = [
   console.log(daysOfTheWeek[0]);
   console.log(daysOfTheWeek[1]);
   console.log(daysOfTheWeek[6]);
-  
+
+  const primeNumbers = [1, 2, 3, 5, 7, 11, 13, 17];
+console.log(primeNumbers.length);
+console.log(primeNumbers.join(" | "));
